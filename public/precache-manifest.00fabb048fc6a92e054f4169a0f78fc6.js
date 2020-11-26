a2d1ce048e44dcdc40e4285758ee950851ac1a3a
Claudia D <claudia.do.gallego@gmail.com>
2020-11-26 13:32:10 +0100

    espero ultimo

---
+++ public/precache-manifest.00fabb048fc6a92e054f4169a0f78fc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3956536f82e46ff591f312e81bd682a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab55a63449a0bcbb50b7",
+    "url": "/static/css/main.d90bdc67.chunk.css"
+  },
+  {
+    "revision": "709ac91627cd517ac070",
+    "url": "/static/js/2.43becca3.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/static/js/2.43becca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab55a63449a0bcbb50b7",
+    "url": "/static/js/main.b4031f93.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "70607f78ce303c1c52642319a01cc320",
+    "url": "/static/media/LoginBackgorund.70607f78.jpg"
+  },
+  {
+    "revision": "750080cfe3006d084d34320ee3115b88",
+    "url": "/static/media/hojitas.750080cf.jpg"
+  },
+  {
+    "revision": "c36dc267a9656d4f8afc6d32eec1e32b",
+    "url": "/static/media/pfBack4.c36dc267.jpg"
+  }
+]);
